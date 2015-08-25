e9621fea7287ccbc6f4889c36a2ff6ce7e0d4295
Jeffery Aronhalt <jaronhalt@pertino.com>
2015-08-25 10:34:23 -0700

    dumb timer

---
+++ timer.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# the above line tells the shell what to invoke this file with
+import time
+import os
+import getpass
+import sys
+
+DIR_NAME = '.timers'
+TIMER_DIR = '/{}/{}/{}'.format(
+                'home'	if sys.platform.startswith('linux')
+                        else 'Users'
+                , getpass.getuser()
+                , DIR_NAME)
+
+# make /Users/<username>/.timers directory
+if not os.path.exists(TIMER_DIR):
+    os.makedirs(TIMER_DIR)
+
+
+def print_usage():
+    print 'usage: timer.py <timer> [start|stop]'
+
+if __name__ == '__main__':
+    # sys.argv is a list of strings: <filename> <timer> <start|stop>
+    if len(sys.argv) < 3:
+        print_usage()
+        exit()
+
+    # check valid command
+    if sys.argv[2] not in ('start', 'stop'):
+        print_usage()
+        exit()
+
+    # get the timer name
+    timer = sys.argv[1]
+    if sys.argv[2] == 'start':
+        start_date = time.strftime("%m/%d/%Y %H:%M:%S")
+        # overwrite the timer's file contents with start date and timestamp
+        open(timer, 'w').write('{}, {}'.format(start_date, time.time()))
+        print 'Timer<{}> started at {}'.format(timer, start_date)
+
+    else:
+        # if it doesn't exist then it hasn't been started yet
+        if not os.path.exists('{}/{}'.format(TIMER_DIR, timer)):
+            print 'Timer<{}> does not exist in {}'.format(timer, TIMER_DIR)
+            exit()
+
+        # it does exists, so read it and parse
+        with open(timer, 'r') as f:
+            # start timestamp
+            start = float(f.read().split(', ')[1])
+
+            # duration
+            dur = int(time.time() - start)
+
+            # divide dur by 60, and mod (%) it by 60
+            m, s = divmod(dur, 60)
+            h, m = divmod(m, 60)
+
+            # pretty formatting, eh?
+            clock = '{}:{}:{}'.format(h, m, s)
+            print 'Timer<{}> duration {}'.format(timer, clock)
+
+            # try to remove the file, just stuff the error
+            try:
+                os.remove('{}/{}'.format(TIMER_DIR, timer))
+            except:
+                pass
